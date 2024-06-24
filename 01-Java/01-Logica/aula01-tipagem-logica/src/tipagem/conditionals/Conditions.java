9371ab1584bcc1b06e48a97e95fec5e403bfc9ca
PauloPeixotoo <paulopeixotoo@hotmail.com>
2024-06-24 17:01:44 -0300

    feat: desenvolvimento logica

--- 01-Java/01-Logica/aula01-tipagem-logica/src/tipagem/conditionals/Conditions.java
+++ 01-Java/01-Logica/aula01-tipagem-logica/src/tipagem/conditionals/Conditions.java
@@ -4,19 +4,19 @@ public class Conditions {
 
     public void exampleIfElse(int Ana) {
         if (Ana > 18) {
-            System.out.println("Ana é maior de idade");
+            System.out.println("Ana é maior de idade.");
         } else {
-            System.out.println("Ana é menor de idade");
+            System.out.println("Ana é menor de idade.");
         }
     }
 
     public void exampleIfElseifElse(int PaFuncio) {
         if (PaFuncio < 14) {
-            System.out.println("Ele é adolescente, só tem 14");
+            System.out.println("Ele é adolescente, só tem 14.");
         } else if (PaFuncio < 16) {
-            System.out.println("Pafuncio é adolescente, ele tem 16");
+            System.out.println("Pafuncio é adolescente, ele tem 16.");
         } else {
-            System.out.println("Ele tem 15 anos");
+            System.out.println("Ele tem 15 anos.");
         }
     }
 }
