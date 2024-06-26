d9694e123c6d89aa301de0df3186709bc292011f
Erick Freitas <erickelsas@alunos.utfpr.edu.br>
2024-06-26 18:31:07 -0300

    ft: validações

--- 01-Java/02-JavaPoo/aula01-task/src/GerenciadorBanco.java
+++ 01-Java/02-JavaPoo/aula01-task/src/GerenciadorBanco.java
@@ -6,13 +6,19 @@ public class GerenciadorBanco {
     }
 
     public void adicionarConta(ContaBancaria conta){
+        if(buscarConta(conta.getnumeroConta) != null){
+            System.out.println("A conta já existe, tente outro número!");
+            return;
+        }
+
         contas.add(conta);
+        System.out.println("Conta adicionada com sucesso!");
     }
 
     public ContaBancaria buscarConta(String numeroConta){
         for(int i = 0; i < contas.size(); i++){
-            if(contas[i].numeroConta.matches(numeroConta)){
-                return contas[i];
+            if(contas.get(i).numeroConta.matches(numeroConta)){
+                return contas.get(i);
             }
         }
 
@@ -21,15 +27,20 @@ public class GerenciadorBanco {
 
     public void removerConta(String numeroConta){
         ContaBancaria conta = buscarConta(numeroConta);
+        if(conta == null){
+            System.out.println("A conta que deseja remover não existe!");
+            return;
+        }
         contas.remove(conta);
+        System.out.println("Conta removida com sucesso!");
     }
 
     public void listarContas(){
         System.out.println("Contas registradas: ");
         for(int i = 0; i < contas.size(); i++){
-            System.out.println("Número da conta: " + contas[i].numeroConta);
-            System.out.println("Titular: " + contas[i].titular);
-            System.out.println("Saldo: " + contas[i].saldo);
+            System.out.println("Número da conta: " + contas.get(i).numeroConta);
+            System.out.println("Titular: " + contas.get(i).titular);
+            System.out.println("Saldo: " + contas.get(i).saldo);
             System.out.println();
         }
     }
