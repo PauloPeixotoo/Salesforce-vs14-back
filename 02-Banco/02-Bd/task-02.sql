01850c566f30e23658ddad1c3e399f712012e6dd
PauloPeixotoo <paulopeixotoo@hotmail.com>
2024-07-08 13:53:35 -0300

    feat: adicao do exercicios comentados

--- 02-Banco/02-Bd/task-02.sql
+++ 02-Banco/02-Bd/task-02.sql
@@ -301,20 +301,33 @@ VALUES (SEQ_ENDERECO.NEXTVAL, (SELECT id_bairro FROM BAIRRO WHERE nome = 'Elmwoo
 
 SELECT * FROM ENDERECO
 
+------------------------------------------------------TASK 02------------------------------------------------------
+
+-- Selecione todos os países ordenados por nome decrescente
+
 SELECT id_pais, nome FROM PAIS ORDER BY nome DESC;
+
+-- Selecionar loradouro e cep dos endereços, Porem os logradouros que comecem com a letra 'a'(maiúsculo ou minúsculo)
 
 SELECT logradouro, cep FROM ENDERECO WHERE logradouro LIKE 'A%' OR logradouro LIKE 'a%';
 
+-- Selecionar todos os endereços que tenham cep com final '0';
+
 SELECT * FROM ENDERECO WHERE cep LIKE '%0';
 
-SELECT NUMERO FROM ENDERECO;
+-- Selecionar todos os endereços que tenham números entre 1 e 100;
 
 SELECT * FROM ENDERECO WHERE numero BETWEEN 1 AND 100;
 
+-- Selecionar todos os endereços que comecem por "RUA" e ordenar pelo cep de forma decrescente;
 
 SELECT * FROM ENDERECO WHERE logradouro LIKE 'Rua%' OR logradouro LIKE 'RUA%' ORDER BY cep DESC;
 
+-- Selecionar a quantidade de endereços cadastrados na tabela;
+
 SELECT COUNT(*) AS quantidade_enderecos FROM ENDERECO;
+
+-- Selecionar a quantidade de endereços cadastrados agrupados pelo id da cidade;
 
 SELECT id_cidade, COUNT(*) AS quantidade_enderecos FROM ENDERECO GROUP BY id_cidade;
 
